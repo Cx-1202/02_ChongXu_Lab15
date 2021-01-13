1be9756021b31016e86de4840a320e106bf978ca
Cx-1202 <chongxu2468@gmail.com>
2021-01-13 15:23:07 +0800

    Initial commit

---
+++ Assets/Script/BGM.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    int rand;
+
+    public AudioSource BGM1;
+    public AudioClip[] BGMArr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BGM1 = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rand = Random.Range(0, BGMArr.Length);
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            BGM1.Stop();
+            BGM1.PlayOneShot(BGMArr[rand]);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            BGM1.GetComponent<AudioSource>().volume = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            BGM1.GetComponent<AudioSource>().volume = 0;
+        }
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    int spacecount = 0;
+    int jumpcount =0;
+    int jumpspeed = 10;
+    int gravitymodlfier = 5;
+    int rand;
+
+    public Text JumpText;
+    public AudioClip[] AudioClipArr;
+    public AudioSource Jumpsound;
+    public Rigidbody PlayerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravitymodlfier;
+        PlayerRB.GetComponent<Rigidbody>();
+
+        Jumpsound = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jumping();
+
+        JumpText.GetComponent<Text>().text = "Jump : " + spacecount;
+        rand = Random.Range(0, AudioClipArr.Length);
+    }
+
+    private void jumping()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && jumpcount == 0)
+        {
+            // Jumpsound.Play();
+            Jumpsound.PlayOneShot(AudioClipArr[rand]);
+            PlayerRB.AddForce(Vector3.up * jumpspeed, ForceMode.Impulse);
+            jumpcount++;
+            spacecount++;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            jumpcount = 0;
+        }
+    }
+}
